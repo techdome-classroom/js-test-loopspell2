7f794ec933aebe9a2f166a4b84b671dae122e943
Dactar <kulmc01@gmail.com>
2024-07-26 12:29:01 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,9 +1,5 @@
 const getTotalIsles = function (grid) {
 
-  // if (!grid || grid.length === 0) {
-  //   return 0;
-  // }
-
   const rows = grid.length;
   const cols = grid[0].length;
   let islands = 0;
