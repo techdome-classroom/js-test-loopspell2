5f05bfc560d13cae99d0fb4e63e4f96c4b02fbb0
Dactar <kulmc01@gmail.com>
2024-07-26 12:25:55 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,18 +8,7 @@ const rows = grid.length;
 const cols = grid[0].length;
 let islands = 0;
 
-const island = (i, j) => {
-    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] !== 'L') {
-        return;
-    }
 
-    grid[i][j] = '#'; 
-
-    island(i + 1, j);
-    island(i - 1, j);
-    island(i, j + 1);
-    island(i, j - 1);
-};
 
 for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
