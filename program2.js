d7fa2315d635ad69b436ce1ce5bc4b4a5d0b8412
Dactar <kulmc01@gmail.com>
2024-07-26 12:46:56 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,7 +5,6 @@ const decodeTheRing = function (s, p) {
 
   while (sIndex < s.length && pIndex < p.length) {
     if (p[pIndex] === '*') {
-      // If we're at the end of the pattern, it matches the rest of the string
       if (pIndex === p.length - 1) return true;
 
       // Find the next non-* character in the pattern
