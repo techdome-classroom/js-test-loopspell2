5ad575cfe80cc72c787e2ffadcf73cab15469f42
Dactar <kulmc01@gmail.com>
2024-07-26 12:52:09 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -8,8 +8,8 @@ const decodeTheRing = function (s, p) {
 
       if (pIndex === p.length - 1) return true;
 
-      //kuldeep
-      //kuld*p
+      //kuldeep s
+      //kuld*p  p
       let nextChar = p[pIndex + 1];
       let nextCharIndex = s.indexOf(nextChar, sIndex);
 
